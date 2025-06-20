807a4a69ca280fb1e6fbe7f5c4d7ebc74f949ea7
Irina Ozerova <marta.ilyashenko@gmail.com>
2025-06-20 21:17:31 +0300

    hw done

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -2,11 +2,8 @@ import { QueryClient, HydrationBoundary, dehydrate } from "@tanstack/react-query
 import { fetchNoteById } from "@/lib/api";
 import NoteDetailsClient from "./NoteDetails.client";
 
-type Props = {
-  params: { id: string };
-};
 
-const NoteDetails = async ({ params }: Props) => {
+const NoteDetails = async ({ params }: { params: { id: string } }) =>{
   const id = Number(params.id);
   const queryClient = new QueryClient();
 
